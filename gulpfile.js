62cb904016096918ffb420d67a2ca14691c28620
Eiki Tabayashi <tabayashi@gmail.com>
2017-07-29 01:39:51 +0900

    Update package.json

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,14 @@ var mocha   = require('gulp-mocha');
 var plumber = require('gulp-plumber');
 
 gulp.task('spec', function() {
-  return gulp.src(['spec/**/*.js'], {read: false})
-              .pipe(plumber())
-              .pipe(mocha());
+  return gulp
+    .src(['spec/**/*.js'], {read: false})
+    .pipe(plumber())
+    .pipe(mocha());
 });
 
 gulp.task('watch', function() {
-  gulp.watch([
-    'src/**/*.js',
-    'spec/**/*.js',
-  ], ['spec']);
+  gulp.watch(['src/**/*.js', 'spec/**/*.js'], ['spec']);
 });
 
 gulp.task('default', ['watch']);
--- src/index.js
+++ src/index.js
@@ -1,38 +1,37 @@
 'use strict';
+var stylus = require('stylus');
+var utils  = stylus.utils;
+var nodes  = stylus.nodes;
 var StringScanner = require('StringScanner');
 
 function numberCode(cc) {
-  return "\\00" + cc.toString(16);
+  return '\\00' + cc.toString(16);
 }
 
 function char2numberCode(datum) {
   return numberCode(datum.charCodeAt(0));
 }
 
-function charEntity2numberCode(datum) {
-  var maps = require('./map.json');
-  if (datum in maps) {
-    return numberEntity2numCode(maps[datum]);
-  } else {
-    return datum.split('')
-                .map(function(c) {
-                  return char2numberCode(c);
-                }).join('');
-  }
-}
+// function charEntity2numberCode(datum) {
+//   var maps = require('./map.json');
+//   if (datum in maps) {
+//     return numberEntity2numCode(maps[datum]);
+//   }
+//   return datum.split('')
+//               .map(function(c) {
+//                 return char2numberCode(c);
+//               })
+//               .join('');
+// }
 
 function numberEntity2numCode(datum) {
   return numberCode(datum.replace(/(?:^&#|;$)/g, ''));
 }
 
-module.exports = function(text) {
-  var stylus, utils, nodes, ss, token = [];
-  stylus = require('stylus');
-  utils  = stylus.utils;
-  nodes  = stylus.nodes;
+function numcode(text) {
   utils.assertString(text, 'text');
-  ss = new StringScanner(text.string);
-
+  var ss = new StringScanner(text.string);
+  var token = [];
   while (!ss.eos()) {
     if (ss.scan(/(&\w+?;)/)) {
       token.push(cent2nc(ss.captures()[0]));
@@ -46,4 +45,6 @@ module.exports = function(text) {
   }
   token = new nodes.String(token.join(''));
   return token;
-};
+}
+
+module.exports = numcode;
